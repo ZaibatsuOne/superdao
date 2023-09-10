3dd862cd00452b2910b49c5f7f2439481927725d
Temerov Anton <hurdlelightsick@gmail.com>
2023-09-10 16:06:39 +0700

    navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,11 +1,26 @@
+import { Button } from "../ui/button";
 import { FC } from "react";
 import Image from "next/image";
 import Logo from "@/public/logo.svg";
+import { navbarLink } from "@/data/data";
 
 const Navbar: FC = () => {
   return (
-    <header className="mx-auto container">
-      <Image src={Logo} alt="Superdao logo" />
+    <header className="mx-auto container py-8 flex items-center justify-between">
+      <div className="flex items-center gap-2">
+        <Image src={Logo} alt="Superdao logo" />
+        <h4 className="font-semibold text-2xl">Superdao</h4>
+      </div>
+      <nav>
+        <ol className="flex items-center gap-11">
+          {navbarLink.map((link) => (
+            <li className="link">{link.title}</li>
+          ))}
+        </ol>
+      </nav>
+      <Button variant="secondary" size="sm">
+        Login
+      </Button>
     </header>
   );
 };
---
+++ data/data.ts
@@ -0,0 +1,24 @@
+import { ILink } from "@/types/types";
+
+export const navbarLink: ILink[] = [
+  {
+    title: "Product",
+    link: "#",
+  },
+  {
+    title: "Use cases",
+    link: "#",
+  },
+  {
+    title: "Learn",
+    link: "#",
+  },
+  {
+    title: "About",
+    link: "#",
+  },
+  {
+    title: "Jobs",
+    link: "#",
+  },
+];
