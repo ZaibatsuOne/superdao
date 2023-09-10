bf52f0027b3595fedbaf76ebe7641e7650b56bf7
Temerov Anton <hurdlelightsick@gmail.com>
2023-09-10 20:17:00 +0700

    Add badge for projects banner

--- components/projects-banner.tsx
+++ components/projects-banner.tsx
@@ -4,17 +4,24 @@ import Image from "next/image";
 
 const ProjectsBanner: FC = () => {
   return (
-    <div className="h-[490px] w-full rounded-[30px] bg-black overflow-hidden ">
-      <Image
-        src={Artworks}
-        alt="1"
-        className="animate-move-up repeat-infinite"
-      />
-      <Image
-        src={Artworks}
-        alt="1"
-        className="animate-move-up repeat-infinite"
-      />
+    <div className="relative">
+      <div className="h-[490px] w-full rounded-[30px] bg-black overflow-hidden ">
+        <Image
+          src={Artworks}
+          alt="1"
+          className="animate-move-up repeat-infinite"
+        />
+        <Image
+          src={Artworks}
+          alt="1"
+          className="animate-move-up repeat-infinite"
+        />
+      </div>
+      <div className="absolute -bottom-8 translate-x-1/2 h-14 w-1/2 bg-orange rounded-[100px] border-4 border-black">
+        <p className="font-bold text-lg text-bg translate-y-1/3">
+          1000+ projects launched on Superdao
+        </p>
+      </div>
     </div>
   );
 };
